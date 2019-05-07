e70a7a959014dc0f9bbeec965b33b794fd51288e
Vijay <7822vijay@gmail.com>
2019-05-07 00:50:04 +0000

    added command line arguments

--- download_avatars.js
+++ download_avatars.js
@@ -1,15 +1,26 @@
 var request = require('request');
 var token = require('./secrets');
 var fs = require('fs');
+const gitRepo = {owner: process.argv[2], repo: process.argv[3]};
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+if (!gitRepo.owner) {
+  console.log("Wrong Arguments");
+  return;
+}
+if (!gitRepo.repo) {
+  console.log("Wrong Arguments");
+  return;
+}
+
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request',
-      'Authorization': "token " + token.GITHUB_TOKEN
+      'Authorization': 'token ' + token.GITHUB_TOKEN
     }
   };
 
@@ -19,12 +30,13 @@ function getRepoContributors(repoOwner, repoName, cb) {
   });
 }
 
-getRepoContributors("jquery", "jquery", function (err, result) {
+getRepoContributors(gitRepo.owner, gitRepo.repo, function (err, result) {
   console.log("Errors:", err);
   result.forEach(element => {
     let path = "./avatars/" + element.login + ".jpg";
     downloadImageByURL(element.avatar_url, path);
   });
+  //console.log("downloaded");
 });
 
 function downloadImageByURL(url, filePath) {
